fdf88167a9a164c38b581c5f6588a013a5343fca
Raghad Qadah <raghadqadah99@gmail.com>
2022-05-19 23:29:09 +0300

    Fix CheckOut without any adress

--- screens/CheckOut.js
+++ screens/CheckOut.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'; import { Text, View } from 'react-native';
 import { Picker } from "@react-native-picker/picker";
-import { StyleSheet, SafeAreaView, Image, Alert, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Alert, TouchableOpacity} from 'react-native';
 
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
@@ -8,177 +8,217 @@ import "firebase/compat/firestore"
 var db = firebase.firestore();
 
 export default class CheckOut extends Component {
-    state = { user: {}, address: '', addressDB: [], email: {} };
-    componentDidMount() {
+  state = { user: {}, address: '', addressDB: [], email: {} };
+  componentDidMount() {
 
-        firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged((user) => {
 
-            if (user != null) {
-                this.setState({ user: user });
-                this.setState({ email: user.email });
-            }
-        })
+      if (user != null) {
+        this.setState({ user: user });
+        this.setState({ email: user.email });
+      }
+    })
 
-    }
-    getAddressDBData = () => {
+  }
+  getAddressDBData = () => {
 
-        let AddressInf;
+    let AddressInf;
 
-        db.collection("usersAddresses")
-            .where('email', '==', this.state.email)
-            .get()
-            .then((querySnapshot) => {
-                AddressInf = querySnapshot.docs.map(doc => doc.data());
-                this.setState({ addressDB: AddressInf });
-                // console.log("GET DATA , ",this.state.email);
-            })
+    db.collection("usersAddresses")
+      .where('email', '==', this.state.email)
+      .get()
+      .then((querySnapshot) => {
+        AddressInf = querySnapshot.docs.map(doc => doc.data());
+        this.setState({ addressDB: AddressInf });
+        // console.log("GET DATA , ",this.state.email);
+      })
 
-    }
-    getAddress(item) {
-        return item.country + ',' + item.city + ',' + item.street + ',' + item.moreDescription;//format: Palestine,Ramallah,Irsal street,buliding No. 10;
-    }
+  }
+  getAddress(item) {
+    return item.country + ',' + item.city + ',' + item.street + ',' + item.moreDescription;//format: Palestine,Ramallah,Irsal street,buliding No. 10;
+  }
 
-    getCurrentDate() {
+  getCurrentDate() {
 
-        var date = new Date().getDate();
-        var month = new Date().getMonth() + 1;
-        var year = new Date().getFullYear();
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
 
-        //Alert.alert(date + '-' + month + '-' + year);
-        // You can turn it in to your desired format
-        return date + '-' + month + '-' + year;//format: dd-mm-yyyy;
-    }
+    //Alert.alert(date + '-' + month + '-' + year);
+    // You can turn it in to your desired format
+    return date + '-' + month + '-' + year;//format: dd-mm-yyyy;
+  }
 
-    addOrder(orders) {
-      this.addTest(orders);
-        orders.forEach((obj) => {
-            db.collection("Orders").add({
-                customerName: this.state.user.displayName,
-                customerEmail: this.state.user.email,
-                OrderDate: this.getCurrentDate(),
-                product_name: obj.name,
-                product_provider: obj.provider,
-                product_price: obj.price,
-                product_image: obj.image,
-                product_quantity: obj.quantity,
-                address: this.state.address
-
-            });
-            db.collection("OrdersCheckedOut").add({
-                customerName: this.state.user.displayName,
-                customerEmail: this.state.user.email,
-                OrderDate: this.getCurrentDate(),
-                product_name: obj.name,
-                product_provider: obj.provider,
-                product_price: obj.price,
-                product_image: obj.image,
-                product_quantity: obj.quantity,
-                address: this.state.address
-            })
-                .catch(function (error) {
-                    console.error("Error adding document: ", error);
-                });
-        });
-    }
-
-    addTest(orders) {
-      db.collection("Orders-Packges").add({
+  addOrder(orders) {
+    this.addTest(orders);
+    orders.forEach((obj) => {
+      db.collection("Orders").add({
         customerName: this.state.user.displayName,
-                customerEmail: this.state.user.email,
-                OrderDate: this.getCurrentDate(),
-                address: this.state.address,
-                TotalPrice: this.props.route.params.TotalAmount,
-                OrderProducts:orders
+        customerEmail: this.state.user.email,
+        OrderDate: this.getCurrentDate(),
+        OrderTimestamp: new Date().valueOf(),
+        product_name: obj.name,
+        product_provider: obj.provider,
+        product_price: obj.price,
+        product_image: obj.image,
+        product_quantity: obj.quantity,
+        address: this.state.address
+
+      });
+      db.collection("OrdersCheckedOut").add({
+        customerName: this.state.user.displayName,
+        customerEmail: this.state.user.email,
+        OrderDate: this.getCurrentDate(),
+        product_name: obj.name,
+        product_provider: obj.provider,
+        product_price: obj.price,
+        product_image: obj.image,
+        product_quantity: obj.quantity,
+        address: this.state.address
       })
         .catch(function (error) {
           console.error("Error adding document: ", error);
         });
-  
+    });
+  }
+
+  addTest(orders) {
+    db.collection("Orders-Packges").add({
+      customerName: this.state.user.displayName,
+      customerEmail: this.state.user.email,
+      OrderDate: this.getCurrentDate(),
+      OrderTimestamp: new Date().valueOf(),
+      address: this.state.address,
+      TotalPrice: this.props.route.params.TotalAmount,
+      OrderProducts: orders
+    })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+
+  }
+
+  Success() {
+
+
+    if (this.state.addressDB.length === 0) {
+
+      Alert.alert("Please Add Address");
+    }
+    else if (this.state.address == '') {
+      console.log("Please Select Address");
+      Alert.alert("Please Select Address");
+    }
+    else {
+      this.addOrder(this.props.route.params.products)
+      this.props.route.params.clearCart
+      this.props.navigation.navigate("CardPayment", {
+        TotalAmount: this.props.route.params.TotalAmount,
+      });
     }
 
 
-    render() {
-      // console.log(this.props.route.params.ProviderName," ++++ duha1111");
-      // console.log(this.props.route.params.userName," ++++ duha2222");
+  }
 
 
-        this.getAddressDBData();
-        let pickerItems = this.state.addressDB.map((s, i) => {
-            // console.log(s);
-            return <Picker.Item key={i} value={this.getAddress(s)} label={this.getAddress(s)} />
-        });
-        return (
-          <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-          >
-            <Text>CheckOut Screen</Text>
-            <Picker
-              selectedValue={this.state.address}
-              onValueChange={(value, index) =>
-                this.setState({ address: value })
-              }
-              mode="dropdown" // Android only
-              style={styles.picker}
-            >
-              <Picker.Item label="Please select the address" value="Unknown" />
-              {pickerItems}
-            </Picker>
-            <Text style={styles.text}>Your address: {this.state.address}</Text>
-            <TouchableOpacity
-              style={styles.buttonContainer}
-              onPress={() => {
-                this.addOrder(this.props.route.params.products) &
-                  //Alert.alert('Orders checked out successfully') &
-                 // console.log("checout",this.props.route.params.TotalAmount)&
-                  this.props.route.params.clearCart &
-                  this.props.navigation.navigate("CardPayment", {
-                    TotalAmount: this.props.route.params.TotalAmount,
-               
-                  });
-              }}
-            >
-              <Text
-                style={{
-                  color: "white",
-                  padding: 5,
-                  fontSize: 18,
-                }}
-              >
-                Check out Order
-              </Text>
-            </TouchableOpacity>
+  render() {
+    // console.log(this.props.route.params.ProviderName," ++++ duha1111");
+    // console.log(this.props.route.params.userName," ++++ duha2222");
+
+
+    this.getAddressDBData();
+
+
+    let pickerItems = this.state.addressDB.map((s, i) => {
+      // console.log(s);
+      return <Picker.Item key={i} value={this.getAddress(s)} label={this.getAddress(s)} />
+    });
+    return (
+
+      <View
+        style={{ flex: 1, alignItems: "center", justifyContent: "center", fontWeight: 'bold', }}
+      >
+        {(this.state.addressDB.length === 0)
+          &&
+          <View>
+            <Text style={{
+              fontSize: 14,
+              textAlign: 'center',
+              color: 'red',
+              width: '80%'
+            }}>No addresses Found ,Please try again</Text>
           </View>
-        );
-    }
+        }
+        {/* <Text>CheckOut Screen</Text> */}
+        <Picker
+          selectedValue={this.state.address}
+          onValueChange={(value, index) =>
+            this.setState({ address: value })
+          }
+          mode="dropdown" // Android only
+          style={styles.picker}
+        >
+          <Picker.Item label=" Select Address" value="Unknown" />
+          {pickerItems}
+        </Picker>
+        <Text style={styles.text}>Your address: {this.state.address}</Text>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => {
+            this.Success();
+            //console.log('deleteOffer',this.state.address=='')
+            //&
+            // this.addOrder(this.props.route.params.products) &
+            // this.props.route.params.clearCart &
+            // this.props.navigation.navigate("CardPayment", {
+            //   TotalAmount: this.props.route.params.TotalAmount,
+
+            // }
+            // );
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              padding: 5,
+              fontSize: 18,
+            }}
+          >
+            Check out Order
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        //   backgroundColor: 'yellow'
-    },
-    text: {
-        fontSize: 24,
-    },
-    picker: {
-        marginVertical: 30,
-        width: 300,
-        padding: 10,
-        borderWidth: 1,
-        borderColor: "#666",
-    }, buttonContainer: {
-        // marginBottom: 50,
-        height: 45,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 10,
-        marginTop: 15,
-        width: 250,
-        borderRadius: 30,
-        backgroundColor: "#800C69",
-        color: 'white'
-    },
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    //   backgroundColor: 'yellow'
+  },
+  text: {
+    fontSize: 24,
+  },
+  picker: {
+    marginVertical: 30,
+    width: 300,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#666",
+  }, buttonContainer: {
+    // marginBottom: 50,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginTop: 15,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: "#800C69",
+    color: 'white'
+  },
 
 });
